b8e113e18dada80c3f66aee996fba3df00b7632d
sebastienle <sebastien.lessard@outlook.com>
2018-03-16 23:36:27 -0400

    PoC for equivalent to Python if __name__
    Attempting to simulate the behavior of Python elegant if __name__ == "main" that will enable a module to run its code only if invoked directly, as opposed to being imported as a module.

---
+++ PythonTypeIfMainCheck_DotSourcedFunctionsFile.ps1
@@ -0,0 +1,54 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.145
+	 Created on:   	2018-03-16 9:42 PM
+	 Created by:   	Sebastien
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+function Double() {
+	param (
+		[int]$Number
+	)
+	return $number * 2
+}
+
+function IsMain() {
+	if ($($(Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line).Substring(0, 3).Trim()) -eq ". .") {
+		return $false
+	}
+	else {
+		return $true
+	}
+}
+
+function Main() {
+	[int]$numberToDouble = 100
+	$result = Double -Number $numberToDouble
+	Write-Output "Result is $result"
+}
+
+<#
+Examine output from Invocation based on different scopes
+'*** Global Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Global -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** Local Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Local -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** Private Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** Script Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** 0 (current) Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** 1 (parent) Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 1 -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'*** Global Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Global -ValueOnly | Select -Expand Line) | Out-Host;
+'*** Local Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Local -ValueOnly | Select -Expand Line) | Out-Host;
+'*** Private Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line) | Out-Host;
+'*** Script Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line) | Out-Host;
+'*** 0 (current) Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly | Select -Expand Line) | Out-Host;
+'*** 1 (parent) Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 1 -ValueOnly | Select -Expand Line) | Out-Host;
+#>
+
+if (IsMain -eq $true) {
+	Main
+}
---
+++ PythonTypeIfMainCheck_Main.ps1
@@ -0,0 +1,49 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.145
+	 Created on:   	2018-03-16 9:41 PM
+	 Created by:   	Sebastien
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+# Dot sourced function file
+. .\PythonTypeIfMainCheck_DotSourcedFunctionsFile.ps1
+
+function Main() {
+	[int]$numberToDouble = 1956
+	$result = Double -Number $numberToDouble
+	Write-Output "Result is $result"
+}
+
+function IsMain() {
+	if ($($(Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line).Substring(0, 3).Trim()) -eq ". .") {
+		return $false
+	} else {
+		return $true
+	}
+}
+
+<#
+Examine output from Invocation based on different scopes
+'=== Global Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Global -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== Local Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Local -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== Private Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== Script Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== 0 (current) Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== 1 (parent) Invocation.MyCommand from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 1 -ValueOnly | Select -Expand MyCommand) | Out-Host;
+'=== Global Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Global -ValueOnly | Select -Expand Line) | Out-Host;
+'=== Local Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Local -ValueOnly | Select -Expand Line) | Out-Host;
+'=== Private Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line) | Out-Host;
+'=== Script Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope Script -ValueOnly | Select -Expand Line) | Out-Host;
+'=== 0 (current) Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly | Select -Expand Line) | Out-Host;
+'=== 1 (parent) Invocation.Line from Main: [{0}]' -f (Get-Variable -Name MyInvocation -Scope 1 -ValueOnly | Select -Expand Line) | Out-Host;
+#>
+
+if (IsMain -eq $true) {
+	Main
+}
